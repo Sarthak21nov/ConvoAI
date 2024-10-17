b4d64901b9c44ed3bbbd381a9668d37d9db0fd72
Sarthak Yash Srivastava <sarthak.nov21@gmail.com>
2024-10-18 00:26:12 +0530

    Completed Backend for putting and getting the Review from Database

--- Backend/Controllers/ReviewController.js
+++ Backend/Controllers/ReviewController.js
@@ -1,8 +1,32 @@
-/* eslint-disable no-unused-vars */
-export const getAllReviews = (req,res)=>{
+import Review from "../Models/ReviewModel.js"
 
+
+/* eslint-disable no-unused-vars */
+export const getAllReviews = async (req,res)=>{
+    try{
+        const reviews = await Review.find({})
+        res.json({status: true, message: "Successfully found all the reviews", data: reviews})
+    } catch(err){
+        res.json({status: false, message: "Unable to get the reviews"})
+    }
 }
 
-export const putReview = (req,res)=>{
+export const putReview = async (req,res)=>{
+    try{
+        const {User_Review, Date, Month, Year} = req.body
+        if(!User_Review || !Date || !Month || !Year) {
+           return res.json({status: false, message: "Didn't received all the required Parameters"})
+        }
+        await Review.create({
+            ReviewMessage: User_Review,
+            Date,
+            Month,
+            Year
+        })
+        return res.json({status: true, message: "Successfully posted the review"})
+    } catch(err){
+        return res.json({status: false, message: "Unable to put the Review"})
+    }
     
+
 }
---
+++ Backend/Models/ReviewModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema({
+    ReviewMessage: {
+        type: "String",
+        required: true
+    },
+    Date : {
+        type: "Number",
+        required: true
+    },
+    Month: {
+        type: "String",
+        required: true
+    },
+    Year: {
+        type: "Number",
+        required: true
+    }
+})
+const Review = mongoose.model('Review', ReviewSchema)
+export default Review
